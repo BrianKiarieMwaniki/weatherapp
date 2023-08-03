b7fd6e58745970c49f47c8e6f2ee4d4e27c2eb73
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-08-03 21:02:50 +0300

    added more weather data for display

--- src/features/RealTimeWeather.jsx
+++ src/features/RealTimeWeather.jsx
@@ -4,6 +4,13 @@ import { getLocation } from "../functions/location";
 
 export const RealTimeWeather = () => {
   const [condition, setCondition] = useState({ description: "", icon: " " });
+  const [location, setLocation] = useState({
+    name: "",
+    region: "",
+    country: "",
+  });
+  const [tempInfo, setTempInfo] = useState({ tempC: 0, tempF: 0 });
+  const [windInfo, setWindInfo] = useState({ windSpeed: 0, windDirection: "" });
   useEffect(() => {
     const { latitude, longitude } = getLocation();
     const getWeather = async () => {
@@ -11,22 +18,48 @@ export const RealTimeWeather = () => {
 
       if (data) {
         const {
+          location: { name, region, country },
           current: {
             condition: { text, icon },
+            temp_c: tempC,
+            temp_f: tempF,
+            wind_kph: windSpeed,
+            wind_dir: windDirection,
           },
         } = data;
 
+        setLocation({ name, region, country });
         setCondition({ description: text, icon });
+        setTempInfo({ tempC, tempF });
+        setWindInfo({ windSpeed, windDirection });
       }
     };
 
     getWeather();
   }, []);
+  const { name, region, country } = location;
   const { description, icon } = condition;
+  const { tempC, tempF } = tempInfo;
+  const { windSpeed, windDirection } = windInfo;
   return (
-    <div>
-      <p>{description}</p>
-      <img src={icon} alt="weather icon" />
+    <div className="real-time">
+      <div className="real-time__location">
+        <p>{name}</p>
+        <p>{region}</p>
+        <p>{country}</p>
+      </div>
+      <div className="real-time__condition">
+        <p>{description}</p>
+        <img src={icon} alt="weather icon" />
+      </div>
+      <div className="real-time__tempInfo">
+        <p>{tempC}</p>
+        <p>{tempF}</p>
+      </div>
+      <div className="real-time__windInfo">
+        <p>{windSpeed}</p>
+        <p>{windDirection}</p>
+      </div>
     </div>
   );
 };
