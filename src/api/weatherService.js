0d5001206a02cc339c4ea79d06a295dc472bf819
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-08-06 16:24:42 +0300

    updated real time weather conditions display

--- src/api/weatherService.js
+++ src/api/weatherService.js
@@ -1,10 +1,29 @@
-import httpActions from './weatherHttpService';
+import httpActions from "./weatherHttpService";
 
-export function getRealTimeWeather(latitude, longitude){
-    const queryParam = {q: `${latitude}, ${longitude}`};
+const baseUri = "https://weather-com.p.rapidapi.com";
 
-    return httpActions.get(
-      "https://weatherapi-com.p.rapidapi.com/current.json",
-      {params: queryParam}
-    );
+export function getRealTimeWeather(latitude, longitude) {
+  const queryParam = { q: `${latitude}, ${longitude}` };
+
+  return httpActions.get("https://weatherapi-com.p.rapidapi.com/current.json", {
+    params: queryParam,
+  });
 }
+
+export const getWeatherForecast = async (latitude, longitude, days) => {
+  const queryParams = { q: `${latitude}, ${longitude}`, days: days };
+
+ const {data} = await httpActions.get(`${baseUri}/forecast.json`, {
+    params: queryParams
+  });
+
+  if( data)
+  {
+     const {location, current, forecast} = data;
+
+     return {location, current, forecast};
+  }
+
+  return null;
+
+};
--- src/features/RealTimeWeather.jsx
+++ src/features/RealTimeWeather.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { getRealTimeWeather } from "../api/weatherService";
+import { getWeatherForecast } from "../api/weatherService";
 import { getLocation } from "../functions/location";
+import { getTodaysDate } from "../functions/date";
 
 export const RealTimeWeather = () => {
   const [condition, setCondition] = useState({ description: "", icon: " " });
@@ -10,34 +11,50 @@ export const RealTimeWeather = () => {
     country: "",
   });
   const [tempInfo, setTempInfo] = useState({ tempC: 0, tempF: 0 });
-  const [windInfo, setWindInfo] = useState({ windSpeedKph: 0, windSpeedMph:0, windDirection: "", windDegree: 0 });
-  const [pressureInfo, setPressureInfo] = useState({pressureMb: 0, pressureIn: 0});
+  const [windInfo, setWindInfo] = useState({
+    windSpeedKph: 0,
+    windSpeedMph: 0,
+    windDirection: "",
+  });
+
+  const [humidity, setHumidity] = useState(0);
+  const [cloud, setCloud] = useState(0);
+
   useEffect(() => {
     const { latitude, longitude } = getLocation();
     const getWeather = async () => {
-      const { data } = await getRealTimeWeather(latitude, longitude);
+      const { location, current } = await getWeatherForecast(
+        latitude,
+        longitude,
+        7
+      );
 
-      if (data) {
-        const {
-          location: { name, region, country },
-          current: {
-            condition: { text, icon },
-            temp_c: tempC,
-            temp_f: tempF,
-            wind_kph: windSpeed,
-            wind_mph: windSpeedMph,
-            wind_dir: windDirection,
-            wind_degree: windDegree,
-            pressure_mb: pressureMb,
-            pressure_in: pressureIn
-          },
-        } = data;
-
+      if (location) {
+        const { name, region, country } = location;
         setLocation({ name, region, country });
+      }
+
+      if (current) {
+        const {
+          condition: { text, icon },
+          temp_c: tempC,
+          temp_f: tempF,
+          wind_kph: windSpeed,
+          wind_mph: windSpeedMph,
+          wind_dir: windDirection,
+          humidity,
+          cloud,
+        } = current;
+
         setCondition({ description: text, icon });
         setTempInfo({ tempC, tempF });
-        setWindInfo({ windSpeedKph: windSpeed,windSpeedMph, windDirection, windDegree });
-        setPressureInfo({pressureMb, pressureIn})
+        setWindInfo({
+          windSpeedKph: windSpeed,
+          windSpeedMph,
+          windDirection,
+        });
+        setHumidity(humidity);
+        setCloud(cloud);
       }
     };
 
@@ -46,41 +63,68 @@ export const RealTimeWeather = () => {
   const { name, region, country } = location;
   const { description, icon } = condition;
   const { tempC, tempF } = tempInfo;
-  const { windSpeedKph: windSpeed, windSpeedMph, windDirection, windDegree } = windInfo;
-  const {pressureMb, pressureIn} = pressureInfo;
+  const { windSpeedKph: windSpeed, windSpeedMph, windDirection } = windInfo;
 
   return (
     <div className="real-time">
       <div className="real-time__main">
         <div className="real-time__location">
-          <p>{name}</p>
-          <p>{region}</p>
+          <p>{name},</p>
+          <p>{region},</p>
           <p>{country}</p>
-        </div>        
+        </div>
+        <div className="real-time__date">
+          <p> {getTodaysDate()}</p>
+        </div>
         <div className="real-time__condition">
-          <p>{description}</p>
-          <img src={icon} alt="weather icon" className="real-time__condition--img"/>
+          <div className="real-time__condition-items">
+            <div className="real-time__condition-item">
+              <svg className="icon">
+                <use xlinkHref="/symbols.svg#icon-thermometer"></use>
+              </svg>
+              <div className="info">
+                <span className="text">Feels like</span>
+                <span className="value">{tempC}&deg;</span>
+              </div>
+            </div>
+            <div className="real-time__condition-item">
+              <svg className="icon">
+                <use xlinkHref="/symbols.svg#icon-wind"></use>
+              </svg>
+              <div className="info">
+                <span className="text">Wind</span>
+                <span className="value">
+                  {windDirection}&nbsp;{windSpeed} km/h
+                </span>
+              </div>
+            </div>
+            <div className="real-time__condition-item">
+              <svg className="icon">
+                <use xlinkHref="/symbols.svg#icon-humidity"></use>
+              </svg>
+              <div className="info">
+                <span className="text">Humidity</span>
+                <span className="value">{humidity}%</span>
+              </div>
+            </div>
+            <div className="real-time__condition-item">
+              <svg className="icon">
+                <use xlinkHref="/symbols.svg#icon-clouds"></use>
+              </svg>
+              <div className="info">
+                <span className="text">Cloud Cover</span>
+                <span className="value">{cloud}%</span>
+              </div>
+            </div>
+          </div>
+          <p className="description">{description}</p>
+          <img
+            src={icon}
+            alt="weather icon"
+            className="real-time__condition--img"
+          />
         </div>
       </div>
-     <div className="real-time__cards">
-        <div className="real-time__card">
-          <p className="title">Temp</p>
-          <p>{tempC}&deg;C</p>
-          <p>{tempF}&deg;F</p>
-        </div>
-        <div className="real-time__card">
-          <p className="title">Wind</p>
-          <p>{windSpeed} kph</p>
-          <p>{windSpeedMph} mph</p>
-          <p>{windDirection}</p>
-          <p>{windDegree}&deg;</p>
-        </div>
-        <div className="real-time__card">
-          <p className="title">Pressure</p>
-          <p>{pressureMb} mb</p>
-          <p>{pressureIn} inHg</p>         
-        </div>
-     </div>
     </div>
   );
 };
